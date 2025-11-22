5b857e01b89a4383aa6a030b241e6dd9af0e0fca
Nnatvron <natravelsitra07@gmail.com>
2025-11-22 11:16:33 +0700

    Thirdty Test

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,24 @@ import Login from './components/Login/Login';
 import Dashboard from './components/Dashboard/Dashboard';
 import ResetForm from './components/Reset/Reset';
 
+import Loader from './components/Loader/Loader';  // Tambahin Loader
+import { useState, useEffect } from 'react';
+
 function App() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // Durasi loader (boleh ubah)
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  // Kalau masih loading → tampilkan Loader dulu
+  if (loading) return <Loader />;
+
   return (
     <AuthProvider>
       <BrowserRouter>
---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Loader.css";
+
+export default function Loader() {
+  return (
+    <div className="loader-container">
+        <img src="/picture/bsi.png.png" alt="UBSI Logo" className="loader-logo" />
+      <div className="loader-text">UBSI ONE+</div>
+    </div>
+  );
+}
